d92df2093e2fccaf2f46cd9abdc5f02638beb097
aetheric-typewriter <sfa95@icloud.com>
2019-04-16 05:51:33 -0400

    Work on mancala

--- w2d2/mancala/lib/board.rb
+++ w2d2/mancala/lib/board.rb
@@ -2,6 +2,8 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+	@name2 = name2
     self.place_stones
   end
 
@@ -20,11 +22,39 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    temp = cups[start_pos]
+    cups[start_pos] = []
     
+    opponent_store_map = {@name1 => 13, @name2 => 6}
+	opponent_store = opponent_store_map[current_player_name]
+    
+    i = (start_pos + 1) % 14
+    
+    until temp.empty?
+	  unless i == opponent_store
+	    cups[i] << temp.pop
+	  end
+	  i = (i + 1) % 14
+    end
+	
+	self.render
+	
+	i = (i - 1) % 14
+	player_store_map = {@name1 => 6, @name2 => 13}
+	return :prompt if i == player_store_map[current_player_name]
+	
+	next_turn(i)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+	i = ending_cup_idx
+	
+	if cups[i].empty?
+	  return :switch
+	else
+	  return i
+	end
   end
 
   def render
