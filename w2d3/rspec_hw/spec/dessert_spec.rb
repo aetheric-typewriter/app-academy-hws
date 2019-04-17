81322ca39d3cef2b2f758218693717aabc6f0604
aetheric-typewriter <sfa95@icloud.com>
2019-04-17 08:28:28 -0400

    Work on hw

--- w2d3/rspec_hw/spec/dessert_spec.rb
+++ w2d3/rspec_hw/spec/dessert_spec.rb
@@ -6,20 +6,37 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
+  subject(:cake) { Dessert.new("cake", 5, chef) }
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do 
+		expect(cake.type).to eq("cake")
+	end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+		expect(cake.quantity).to eq(5)
+	end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+		expect(cake.ingredients.empty?).to eq(true)
+	end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+		expect { Desert.new("cake", "a few bits", chef) }.to raise_error(ArgumentError)
+	end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+	context "when an ingredient is added" do
+		before(:each) do
+			cake.add_ingredient("vanilla")
+		end
+		
+		it "adds an ingredient to the ingredients array" do
+			expect(cake.ingredients.last).to eq("vanilla")
+		end
+	end
   end
 
   describe "#mix!" do
@@ -27,8 +44,16 @@ describe Dessert do
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-
+	context "when a desert is eaten" do
+		before(:each) do
+			cake.eat(1)
+		end
+		
+		it "subtracts an amount from the quantity" do
+			expect(cake.quantity).to eq(4)
+		end
+	end
+	
     it "raises an error if the amount is greater than the quantity"
   end
 
